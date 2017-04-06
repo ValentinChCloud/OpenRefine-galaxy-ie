22197bdecbc738829843083c5e0879cfd876a38a
ValentinChcloud <chavaldranis@gmail.com>
2017-04-06 17:37:34 +0200

    OpenRefine Image working

--- startup.sh
+++ startup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #sed -i "s|PROXY_PREFIX|${PROXY_PREFIX}|" /proxy.conf;
-#cp /proxy.conf /etc/nginx/sites-enabled/default;
+cp /proxy.conf /etc/nginx/sites-enabled/default;
 
 # Here you would normally start whatever service you want to start. In our
 # example we start a simple directory listing service on port 8000
 
-exec ../OpenRefine/refine -i 0.0.0.0 -d /mnt/refine 
+exec ../OpenRefine/refine -i 0.0.0.0 -p 80 -d /mnt/refine 
 
 
 # Launch traffic monitor which will automatically kill the container if traffic
 # stops
-#/monitor_traffic.sh &
-# And nginx in foreground mode.
-#nginx -g 'daemon off;'
+/monitor_traffic.sh &
+#And nginx in foreground mode.
+nginx -g 'daemon off;'
