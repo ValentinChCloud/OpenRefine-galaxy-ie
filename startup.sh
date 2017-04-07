f0279e849db7a9a088af447d1b1409825b82fe04
ValentinChcloud <chavaldranis@gmail.com>
2017-04-07 16:21:22 +0200

    Add GIE{config,static,templates} folders

--- startup.sh
+++ startup.sh
@@ -6,6 +6,8 @@ cp /proxy.conf /etc/nginx/sites-enabled/default;
 # Here you would normally start whatever service you want to start. In our
 # example we start a simple directory listing service on port 8000
 
+#By default (and for security reasons) Refine only listens to TCP requests coming from localhost (127.0.0.1 on port 3333) . If you want to respond to TCP request coming to any Ip adress the machine has, #run refine like this from the command line :
+#./refine -i 0.0.0.0
 exec ../OpenRefine/refine -i 0.0.0.0 -p 80 -d /mnt/refine 
 
 
