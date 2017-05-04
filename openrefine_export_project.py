5eef0e5fd891805fb7c160fe7ce85c37cbef0fd7
ValentinChcloud <chavaldranis@gmail.com>
2017-05-04 16:04:27 +0200

    Second attempt , put function works

--- openrefine_export_project.py
+++ openrefine_export_project.py
@@ -1,15 +1,21 @@
 import sys
 import os
 
+#API to help interactive environnement get data from galaxy
+from galaxy_ie_helpers import put
+
+
 
 sys.path.append("refine.py")
 import refine
-
 r = refine.Refine()
 p = r.new_project("dates.txt")
 #p.apply_operations("operations.json")
 localefile = open("fichier_to_get.txt",'w')
 localefile.write(p.export_rows())
+localefile.close()
 put("fichier_to_get.txt", file_type='txt')
+
+
 
 
