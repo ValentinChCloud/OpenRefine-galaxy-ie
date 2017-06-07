632c0e2bd4ecc1a158b368b6e121285d8afe34f9
ValentinChcloud <chavaldranis@gmail.com>
2017-06-07 10:12:18 +0200

    Add shebang to openrefine_create_project_API

--- openrefine_create_project_API.py
+++ openrefine_create_project_API.py
@@ -1,6 +1,6 @@
+#/usr/bin/python
 import sys
 import os
-
 
 sys.path.append("refine.py")
 import refine
@@ -9,11 +9,12 @@ r = refine.Refine()
 p = r.new_project(sys.argv[1])
 cwd = os.getcwd()
 
-	try:
-		os.mkdir(cwd+'/temp_dossier')
-	except OSError:
-		pass
-		with open(cwd+'/temp_dossier/project_openrefine','wb') as fichier :
+sys.stdout.write("Importing data from galaxy into openrefine")
+try:
+	os.mkdir(cwd+'/temp_dossier')
+except OSError:
+	pass
+	with open(cwd+'/temp_dossier/project_openrefine','wb') as fichier :
 		mon_pickler = pickle.Pickler(fichier)
 		mon_pickler.dump(p)
 
